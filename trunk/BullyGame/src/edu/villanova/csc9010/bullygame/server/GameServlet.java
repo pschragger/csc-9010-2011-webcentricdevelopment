f037cd55b1ec34549b0628e8ea2ad2c15e753fea
Takashi binns <Takashi.binns@gmail.com>
2011-02-28 18:36:30 -0500

    added functions to get game info, draw a card, use a card, and check the game state

--- trunk/BullyGame/src/edu/villanova/csc9010/bullygame/server/GameServlet.java
+++ trunk/BullyGame/src/edu/villanova/csc9010/bullygame/server/GameServlet.java
@@ -10,52 +10,187 @@ public class GameServlet  extends HttpServlet
 
 	private static final long serialVersionUID = 1L;
 	JSONParser parser = new JSONParser();
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-              throws IOException
-    {
-        //set response error by default, change if content != null
-        String returnString = "Error - content was null";
-        response.setContentType("application/xhtml+xml");
-
-        //get raw content from request
-        request.setCharacterEncoding("UTF-8");
-        String strJSON = request.getParameter("content");
-
-        //parse the raw content into a json object
+	
+	private JSONObject parseJSON(String jsonContent)
+	{
+		//parse the raw content into a json object
         Object obj;
         JSONObject jsonObj = null;
         try
         {
-            obj = parser.parse(strJSON);
+            obj = parser.parse(jsonContent);
             jsonObj = (JSONObject)obj;
         }
         catch (Exception ex)
         {
+        	System.out.println("Error parsing JSON object");
         	System.err.println(ex);
         }
 
-        //make sure json object is not null
-        if (jsonObj!=null)
-        {
-            String Data = null;
-            try
-            {
-            	//append the text " was parsed" to the data
-                Data = jsonObj.get("Data").toString() + " was parsed"; 
-            }
-            catch (Exception e)
-            {
-            	//if there was an error, 
-                Data = "data error";
-            }
-            
-            //put data back into the json
-            jsonObj.put("Data", Data);
-            returnString = jsonObj.toJSONString();
-        }
+        return jsonObj;
+	}
+	
+	private String getGameInfo()
+	{
+		String GameID = "100";
+		String PlayerID = "1";
+		String TurnNumber = "3";
+		JSONObject response = new JSONObject();
+		
+		response.put("GameID", GameID);
+		response.put("PlayerID", PlayerID);
+		response.put("TurnNumber", TurnNumber);
+		
+		return response.toJSONString();
+	}
 
+	private String getCard(String jsonContent)
+	{
+		String CardURL = "http://127.0.0.1:8888/images/cards/card1.png";
+		String DiceRoll1 = "3";
+		String DiceRoll2 = "5";
+		JSONObject response = new JSONObject();
+		
+		response.put("CardURL", CardURL);
+		response.put("DiceRoll1", DiceRoll1);
+		response.put("DiceRoll2", DiceRoll2);
+		
+		return response.toJSONString();
+	}
+
+	private String useCard(String jsonContent)
+	{
+		//parse the string into a json object
+		JSONObject json = parseJSON(jsonContent);
+		String GameID = json.get("GameID").toString();
+		String PlayerID = json.get("PlayerID").toString();
+		String CardNumber = json.get("CardNumber").toString();
+
+		//check to see if proposed move is valid
+		String Response = null;
+		boolean ValidMove = true;			//should have a real check here!
+		if (ValidMove)
+		{
+			//update the game state
+			
+			Response = "{\"Status\":\"Valid\"}";
+		}
+		else
+		{
+			//still the current user's turn
+			Response = "{\"Status\":\"Invalid\"}";
+		}
+		
+		return Response;
+	}
+    
+	private String checkState(String jsonContent)
+	{
+		//parse the string into a json object
+		JSONObject json = parseJSON(jsonContent);
+		String GameID = json.get("GameID").toString();
+		String PlayerID = json.get("PlayerID").toString();
+		String TurnNumber = json.get("TurnNumber").toString();
+
+		//check to see if proposed move is valid
+		String Response = null;
+		boolean SameState = true;			//should have a real check here!
+		if (SameState)
+		{
+			//update the game state
+			
+			Response = "{\"Status\":\"NoChange\"}";
+		}
+		else
+		{
+			//still the current user's turn
+			Response = "{\"Status\":\"State Changed\"}";
+		}
+		
+		return Response;
+	}
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+              throws IOException
+    {
+    	//set response error by default, change if content != null
+        String returnString = "Error - content was null";
+        response.setContentType("application/xhtml+xml");
+
+        String Command = request.getParameter("action");
+        if (Command==null)
+        {
+        	System.err.println("no action defined");
+        	returnString = "no action defined.";
+        }
+        else if (Command.compareTo("GetGameInfo")==0)
+        {
+        	System.err.println("getting game info");
+            returnString = getGameInfo();
+        }
+        else if (Command.compareTo("GetCard")==0)
+        {
+        	System.err.println("getting a new card");
+        	String jsonContent = request.getParameter("content");
+            returnString = getCard(jsonContent);
+    	}
+        else if (Command.compareTo("UseCard")==0)
+        {
+        	System.err.println("using a card");
+        	String jsonContent = request.getParameter("content");
+            returnString = useCard(jsonContent);
+    	}
+        else if (Command.compareTo("CheckState")==0)
+        {
+        	System.err.println("checking for a new state");
+        	String jsonContent = request.getParameter("content");
+            returnString = checkState(jsonContent);
+    	}
+        /*
+        else
+        {
+        	
+	        //get raw content from request
+	        request.setCharacterEncoding("UTF-8");
+	        String strJSON = request.getParameter("content");
+	
+	        //parse the raw content into a json object
+	        Object obj;
+	        JSONObject jsonObj = null;
+	        try
+	        {
+	            obj = parser.parse(strJSON);
+	            jsonObj = (JSONObject)obj;
+	        }
+	        catch (Exception ex)
+	        {
+	        	System.out.println("here!");
+	        	System.err.println(ex);
+	        }
+	
+	        //make sure json object is not null
+	        if (jsonObj!=null)
+	        {
+	            String Data = null;
+	            try
+	            {
+	            	//append the text " was parsed" to the data
+	                Data = jsonObj.get("Data").toString() + " was parsed"; 
+	            }
+	            catch (Exception e)
+	            {
+	            	//if there was an error, 
+	                Data = "data error";
+	            }   
+	            //put data back into the json
+	            jsonObj.put("Data", Data);
+	            returnString = jsonObj.toJSONString();
+	        }
+        }
+        */
+        
         //write the return string to the printer
+        System.out.println("Send this json - " + returnString);
         PrintWriter out = response.getWriter();
         out.printf(returnString);
         out.close();
--- trunk/BullyGame/war/js/ClientActions.js
+++ trunk/BullyGame/war/js/ClientActions.js
@@ -34,9 +34,249 @@ function sendData(DisplayID, DataID)
       console.log("data = " + strJSON);
       
       //create and send the http request
-      var QueryString = "content=" + strJSON;
+      var QueryString = "action=else&content=" + strJSON;
       req.open("GET", "http://127.0.0.1:8888/bullygame/game?" + QueryString, true);
       req.send(null);
+     
+    }
+
+    //create a new request & set its event handler
+    req = new XMLHttpRequest();
+    req.onreadystatechange = servletCallback;
+    
+    //send the request
+    callServlet();
+}
+
+function getGameInfo()
+{
+	var GameID = Cookie.read("gameid");
+    var PlayerID = Cookie.read("playerid");
+    var TurnNumber = Cookie.read("turnnum");
+    if ((GameID==null) || (PlayerID==null))
+	{
+    	function servletCallback()
+        {
+        	//make sure ready state = 4 and status = 200 (OK)
+        	if ((req.readyState==4) &&(req.status==200))
+            {
+                console.log("cookies received...");
+                
+                //decode the json and put it in an object
+                var receivedJSON = JSON.decode(req.responseText);
+                
+                //create the new cookies
+                GameID = Cookie.write("gameid", receivedJSON.GameID);
+                PlayerID = Cookie.write("playerid", receivedJSON.PlayerID);
+                TurnNumber = Cookie.write("turnnum", receivedJSON.TurnNumber);
+            }
+        }
+        
+    	function callServlet()
+        {
+          //create and send the http request
+          var QueryString = "action=GetGameInfo";
+          req.open("GET", "http://127.0.0.1:8888/bullygame/game?" + QueryString, true);
+          req.send(null);
+         
+        }
+
+        //create a new request & set its event handler
+        req = new XMLHttpRequest();
+        req.onreadystatechange = servletCallback;
+        
+        //send the request
+        callServlet();
+    	
+	}
+}
+
+function checkCookies()
+{
+	//if cookies not found, ask servlet for game id and player id
+	var GameID = Cookie.read("gameid");
+    var PlayerID = Cookie.read("playerid");
+    var TurnNumber = Cookie.read("turnnum");
+	if ((GameID==null) || (PlayerID==null))
+	{
+		getGameInfo();
+	}
+}
+
+function getCard()
+{
+    function servletCallback()
+    {
+    	//make sure ready state = 4 and status = 200 (OK)
+    	if ((req.readyState==4) &&(req.status==200))
+        {
+            console.log("callback received...");
+            
+            //decode the json and put it in an object
+            var receivedJSON = JSON.decode(req.responseText);
+            
+            //create the new html from the json's data
+            var CardURL = receivedJSON.CardURL;
+            var DiceRoll1 = receivedJSON.DiceRoll1;
+            var DiceRoll2 = receivedJSON.DiceRoll2;
+            
+            //display a new card
+            var NewCard = document.getElementById("NewCard");
+            NewCard.src = CardURL;
+            
+            //update the dice
+            var Dice1 = document.getElementById("Dice1");
+            var Dice2 = document.getElementById("Dice2");
+            Dice1.innerHTML = DiceRoll1;
+            Dice2.innerHTML = DiceRoll2;
+            }
+    }
+
+    function callServlet()
+    {
+      //get the session variables
+      checkCookies();
+      var GameID = Cookie.read("gameid");
+      var PlayerID = Cookie.read("playerid");
+            
+      //create the JSON object
+      var objJSON =
+      {
+        "GameID" : GameID,
+        "PlayerID" : PlayerID
+      };
+
+      var strJSON = JSON.encode(objJSON);
+      
+      //create and send the http request
+      var QueryString = "action=GetCard&content=" + strJSON;
+      req.open("GET", "http://127.0.0.1:8888/bullygame/game?" + QueryString, true);
+      req.send(null);
+     
+    }
+
+    //create a new request & set its event handler
+    req = new XMLHttpRequest();
+    req.onreadystatechange = servletCallback;
+    
+    //send the request
+    callServlet();
+}
+
+function useCard(CardNumber)
+{
+	function servletCallback()
+    {
+    	//make sure ready state = 4 and status = 200 (OK)
+    	if ((req.readyState==4) &&(req.status==200))
+        {
+            console.log("useCard callback received...");
+            
+            //decode the json and put it in an object
+            var receivedJSON = JSON.decode(req.responseText);
+            
+            //create the new html from the json's data
+            var Status = receivedJSON.Status
+            
+            //display a new card
+            if (Status=="Valid")
+        	{
+            	alert("roll was validated by the server.")
+        	}
+        }
+    }
+
+    function callServlet()
+    {
+      //get the session variables
+      checkCookies();
+      var GameID = Cookie.read("gameid");
+      var PlayerID = Cookie.read("playerid");
+      
+      //get pawn moves
+      var pNumberArray = [1];
+      var pStartArray = [0];
+      var pEndArray = [5];
+      
+      //create the JSON object
+      var objJSON =
+      {
+        "GameID" : GameID,
+        "CardNumber" : CardNumber,
+        "PlayerID" : PlayerID,
+        "Moves" : 
+        	{
+        		"PawnNumber" : pNumberArray,
+        		"StartSpace" : pStartArray,
+        		"EndSpace" : pEndArray
+        	}
+      };
+
+      var strJSON = JSON.encode(objJSON);
+      console.log("JSON sent: " + strJSON);
+      
+      //create and send the http request
+      var QueryString = "action=UseCard&content=" + strJSON;
+      req.open("GET", "http://127.0.0.1:8888/bullygame/game?" + QueryString, true);
+      req.send(null);
+     
+    }
+
+    //create a new request & set its event handler
+    req = new XMLHttpRequest();
+    req.onreadystatechange = servletCallback;
+    
+    //send the request
+    callServlet();
+}
+
+function checkState()
+{
+	function servletCallback()
+    {
+    	//make sure ready state = 4 and status = 200 (OK)
+    	if ((req.readyState==4) &&(req.status==200))
+        {
+            console.log("game state check callback received...");
+            
+            //decode the json and put it in an object
+            var receivedJSON = JSON.decode(req.responseText);
+            
+            //create the new html from the json's data
+            var Status = receivedJSON.Status
+            
+            //check to see if something was changed
+            if (Status=="State Changed")
+        	{
+            	alert("There has been a change")
+        	}
+        }
+    }
+
+    function callServlet()
+    {
+      //get the session variables
+      checkCookies();
+      var GameID = Cookie.read("gameid");
+      var PlayerID = Cookie.read("playerid");
+      var TurnNumber = Cookie.read("turnnum")
+            
+      //create the JSON object
+      var objJSON =
+      {
+        "GameID" : GameID,
+        "PlayerID" : PlayerID,
+        "TurnNumber" : TurnNumber
+      };
+
+      var strJSON = JSON.encode(objJSON);
+      console.log("JSON sent: " + strJSON);
+      
+      //create and send the http request
+      var QueryString = "action=CheckState&content=" + strJSON;
+      req.open("GET", "http://127.0.0.1:8888/bullygame/game?" + QueryString, true);
+      req.send(null);
+     
     }
 
     //create a new request & set its event handler
