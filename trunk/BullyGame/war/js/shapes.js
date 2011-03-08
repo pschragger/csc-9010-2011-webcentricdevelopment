509ff372fceea62d4bf60566ef12715864aed3d6
vvuono01 <vvuono01@localhost>
2011-03-08 12:30:28 -0500

    Set up the paths on the board to enable piece movement (no movement yet)

---
+++ trunk/BullyGame/war/js/shapes.js
@@ -0,0 +1,16 @@
+function Square(x, y, size, color)
+{
+	this.x = x;
+	this.y = y;
+	this.size = size;
+	this.color = color;
+	this.isFilled = false;
+}
+
+function Pawn(x, y, size, color)
+{
+	this.x = x;
+	this.y = y;
+	this.size = size;
+	this.color = color;
+}
