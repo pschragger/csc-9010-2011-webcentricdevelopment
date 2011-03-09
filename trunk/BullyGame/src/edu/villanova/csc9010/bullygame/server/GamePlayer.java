f5fe9b32e408aa22bb5809a2b85dcc1f4854d51e
cherian benjamin <cherian.benjamin@gmail.com>
2011-03-09 16:12:29 -0500

    added a new Game constructor

---
+++ trunk/BullyGame/src/edu/villanova/csc9010/bullygame/server/GamePlayer.java
@@ -0,0 +1,79 @@
+/* GamePlayer.java
+ * Data Object to store the gameID and the associate PlayerIDs 
+ * 
+ */
+
+package edu.villanova.csc9010.bullygame.server;
+
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.users.User;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+/*
+ * Data object that stores Game IDs and the Player ID if they are playing that game
+ */
+@PersistenceCapable
+public class GamePlayer {
+	@PrimaryKey
+	@Persistent
+	private Key game;
+	
+	@PrimaryKey
+	@Persistent
+	private User user;
+	
+	/**
+	 * creates a new game and associates players to the game.
+	 * @param pl ArrayList of User objects who are participating in this game.
+	 */
+	public GamePlayer(ArrayList<User> pl)
+	{
+		//Creates a new Game object and uses that key to associate the players to that game
+		Key k;
+		Game gm = new Game();
+		k = gm.getKey();
+		Iterator<User> iter = pl.iterator();
+		while (iter.hasNext())
+		{
+			new GamePlayer(k, iter.next());
+		}
+		
+	}
+	
+	/**
+	 * Associates the game key with the user
+	 * @param g Game key
+	 * @param u User
+	 */
+	public GamePlayer(Key g, User u)
+	{
+		this.game = g;
+		this.user = u;
+	}
+	
+	public Key getGame()
+	{
+		return game;
+	}
+	
+	public User getUser()
+	{
+		return user;
+	}
+	
+	public void setGame(Key g)
+	{
+		game = g;
+	}
+	
+	public void setUser(User u)
+	{
+		user = u;
+	}
+	
+}
