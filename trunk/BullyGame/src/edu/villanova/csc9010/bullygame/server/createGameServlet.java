3024223bf5292f3b0c1bc7708254317980c729fc
cherian benjamin <cherian.benjamin@gmail.com>
2011-03-16 02:37:35 -0400

    Created servlet for game creation. Changed User association from User object to User email. Easier to pass and store a String than an entire object.

---
+++ trunk/BullyGame/src/edu/villanova/csc9010/bullygame/server/createGameServlet.java
@@ -0,0 +1,43 @@
+package edu.villanova.csc9010.bullygame.server;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+import javax.jdo.PersistenceManager;
+import javax.servlet.http.*;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import java.util.ArrayList;
+
+import edu.villanova.csc9010.bullygame.server.GamePlayer;
+import edu.villanova.csc9010.bullygame.server.PMF;
+
+public class createGameServlet extends HttpServlet {
+	private static final Logger log = Logger.getLogger(createGameServlet.class.getName());
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+		//Get current logged in user by email
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+		String cu = user.getEmail();
+		
+		@SuppressWarnings("unchecked")
+		ArrayList<String> pl = (ArrayList<String>) req.getAttribute("users");
+	
+		
+		GamePlayer game = new GamePlayer(pl);
+		
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		try 
+		{
+			pm.makePersistent(game);
+		}
+		finally
+		{
+			pm.close();
+		}
+	}
+	
+
+}
