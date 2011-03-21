7b84b9c16a904b9a581e6d23f5e5c5cd70ae987f
cherian benjamin <cherian.benjamin@gmail.com>
2011-03-21 15:24:03 -0400

    This class does not work as intended and is no longer used, but left in repo in case it is needed later.

--- trunk/BullyGame/src/edu/villanova/csc9010/bullygame/server/GamePlayerKey.java
+++ trunk/BullyGame/src/edu/villanova/csc9010/bullygame/server/GamePlayerKey.java
@@ -1,10 +1,16 @@
 package edu.villanova.csc9010.bullygame.server;
-
+/**
+ * THIS IS NO LONGER USED
+ */
 import java.io.Serializable;
 import com.google.appengine.api.datastore.Key;
 
 public class GamePlayerKey implements Serializable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	public Key game;
 	public String user;
 	
