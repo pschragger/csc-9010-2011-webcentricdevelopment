a9ec4a3ac6e3f1ab75ad7abb6995a43807a8b97d
cherian benjamin <cherian.benjamin@gmail.com>
2011-03-21 15:20:24 -0400

    Automatically assign order to player. Also removed old imports.

--- trunk/BullyGame/src/edu/villanova/csc9010/bullygame/server/createGameServlet.java
+++ trunk/BullyGame/src/edu/villanova/csc9010/bullygame/server/createGameServlet.java
@@ -4,11 +4,7 @@ import java.io.IOException;
 import java.util.logging.Logger;
 import javax.jdo.PersistenceManager;
 import javax.servlet.http.*;
-import com.google.appengine.api.users.User;
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
-import java.util.ArrayList;
-
+import com.google.appengine.api.datastore.Key;
 import edu.villanova.csc9010.bullygame.server.GamePlayer;
 import edu.villanova.csc9010.bullygame.server.PMF;
 
@@ -18,14 +14,32 @@ public class createGameServlet extends HttpServlet {
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		
 		String[] pl = req.getParameterValues("player");
-	
+		int color = 0;
 		
-		GamePlayer game = new GamePlayer(pl);
+	
+		Key gKey;
+		GameState game = new GameState();
+		gKey = game.getKey();
 		
 		PersistenceManager pm = PMF.get().getPersistenceManager();
 		try 
 		{
+			//store GameState
 			pm.makePersistent(game);
+			
+			//store players for game
+			for (String player : pl)
+			{
+				//Automatically assigns player color/order 0 to 3
+				if (color == 3)
+					color = 0;
+				else
+					color++;
+				
+				System.out.println(player);
+				pm.makePersistent(new GamePlayer(gKey, player,color));
+			}
+			
 		}
 		finally
 		{
