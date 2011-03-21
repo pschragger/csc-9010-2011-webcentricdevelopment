26d5ed913d725cd9949a5425c71ecbab0092a6be
cherian benjamin <cherian.benjamin@gmail.com>
2011-03-21 15:25:29 -0400

    Added int color to determine player color and play order.

--- trunk/BullyGame/src/edu/villanova/csc9010/bullygame/server/GamePlayer.java
+++ trunk/BullyGame/src/edu/villanova/csc9010/bullygame/server/GamePlayer.java
@@ -6,6 +6,7 @@
 package edu.villanova.csc9010.bullygame.server;
 
 import com.google.appengine.api.datastore.Key;
+import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
@@ -14,41 +15,37 @@ import javax.jdo.annotations.IdentityType;
 /*
  * Data object that stores Game IDs and the Player ID if they are playing that game
  */
-@PersistenceCapable(identityType = IdentityType.APPLICATION, objectIdClass=GamePlayerKey.class)
+@PersistenceCapable	//(identityType = IdentityType.APPLICATION, objectIdClass=GamePlayerKey.class)
 public class GamePlayer {
 	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Key gpID;
+	
 	@Persistent
 	private Key game;
 	
-	@PrimaryKey
 	@Persistent
 	private String user;
 	
-	/**
-	 * creates a new game and associates players to the game.
-	 * @param pl ArrayList of User emails who are participating in this game.
-	 */
-	public GamePlayer(String[] pl)
-	{
-		//Creates a new Game object and uses that key to associate the players to that game
-		Key k;
-		GameState gm = new GameState();
-		k = gm.getKey();
-		for (String player : pl)
-		{
-			new GamePlayer(k, player);
-		}
-	}
+	@Persistent
+	private int color;
 	
 	/**
 	 * Associates the game key with the user
 	 * @param g Game key
 	 * @param u Email of the user
+	 * @param c Color of the player's pawn. This will determine order. 
 	 */
-	public GamePlayer(Key g, String u)
+	public GamePlayer(Key game, String user, int color)
 	{
-		this.game = g;
-		this.user = u;
+		this.game = game;
+		this.user = user;
+		this.color = color;
+	}
+	
+	public Key getGPID()
+	{
+		return gpID;
 	}
 	
 	public Key getGame()
@@ -61,14 +58,24 @@ public class GamePlayer {
 		return user;
 	}
 	
-	public void setGame(Key g)
+	public int getColor()
 	{
-		game = g;
+		return color;
 	}
 	
-	public void setUser(String u)
+	public void setGame(Key game)
 	{
-		user = u;
+		this.game = game;
+	}
+	
+	public void setUser(String user)
+	{
+		this.user = user;
+	}
+	
+	public void setColor(int color)
+	{
+		this.color = color;
 	}
 	
 }
