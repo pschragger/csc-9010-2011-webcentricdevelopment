e74a043f5fdab5505f0f6790f40f2f05f12ac817
vinay.singh84 <vinay.singh84@localhost>
2011-03-23 16:53:52 -0400

    JSON fixes - Client/Servlet communications

--- trunk/BullyGame/war/js/services.js
+++ trunk/BullyGame/war/js/services.js
@@ -1,136 +1,32 @@
-
-//used to get the game info (player#, game id, turn #) and put them in cookies
-function getGameInfo()
-	// Create then call the Request
-	var myRequest = new Request.JSON({
-		url: '/bullygame/game', 
-		onSuccess: function(responseText){			
-			console.log("cookies received...");
-			
-			var jsonResponse = JSON.decode(responseText);
-			
-            //update the cookies
-            TurnNumber = Cookie.write("turnnum", jsonResponse.TurnNumber);			
-		},
-		onFailure: function(){
-			alert('Error communicating with Server.');	
-		},		
-	});
-	
-	myRequest.set('send', createQueryObj('GetGameInfo',true,true,true));	
-	myRequest.send();
-}
-
-//used to get the game info (player#, game id, turn #) and put them in cookies
-function checkState()
-	// Create then call the Request
-	var myRequest = new Request.JSON({
-		url: '/bullygame/game', 
-		onSuccess: function(responseText){
-            console.log("game state check callback received...");
-            
-            //decode the json and put it in an object
-            var jsonResponse = JSON.decode(responseText);
-            
+//old function...only used by the textarea to demonstrate functionality demo
+function sendData(DisplayID, DataID) {
+	// Create Request
+	var jsonRequest = new Request.JSON({
+	    url: '/bullygame/game',
+	    onSuccess: function(reponseJSON){
             //create the new html from the json's data
-            var Status = jsonResponse.Status;
+            var newHtml = "<p>" + reponseJSON.Data + "</p>";
+            console.log("new html = " + newHtml);
             
-            //update the turn
-            var turnElm = $('turn');
-            turnElm.set('html', jsonResponse.TurnNumber);
-            
-            //check to see if something was changed
-            if (Status=="State Changed") {
-            	alert("There has been a change in state!");
-        	}		
-        },
-		onFailure: function(){
-			alert('Error communicating with Server.');	
-		},
-		
+            //get the div id of a html element, and update its contents
+            var MyDiv = document.getElementById(reponseJSON.DivId);
+            MyDiv.innerHTML=newHtml;
+	    },
+	    onFailure: function(){
+	        alert("Error communicating with server.");
+	    }
 	});
-		
-	
-	myRequest.set('send', createQueryObj('checkState',true,true,true));	
-	myRequest.send();	
+    // Create JSON object
+    var objJSON = {
+      "DivId" : DisplayID,
+      "Data" : $('dataElement').value
+    };
+    // Send Request
+	jsonRequest.get({
+		'action' : 'else',
+		'content' : JSON.encode(objJSON)
+	});   
 }
-
-
-//sends a request to the server to use a card
-function playTurn() {
-	// Create then call the Request
-	var myRequest = new Request.JSON({
-		url: '/bullygame/game', 
-		onSuccess: function(responseText){
-			console.log("useCard callback received...");
-            
-            //decode the json and put it in an object
-            var jsonResponse = JSON.decode(responseText);
-            
-            //create the new html from the json's data
-            var Status = jsonResponse.Status;
-            
-            //display a new card
-            if (Status=="Valid") {
-            	alert("roll was validated by the server.")
-        	}		
-        },
-		onFailure: function(){
-			alert('Error communicating with Server.');	
-		},
-		
-	});
-		
-		//get the session variables
-		      checkCookies();
-		      var GameID = Cookie.read("gameid");
-		      var PlayerID = Cookie.read("playerid");
-		      	
-		      //get pawn moves
-		      var pNumberArray = [1];
-		      var pStartArray = [0];
-		      var pEndArray = [5];
-		      
-		      //create the JSON object
-		      var objJSON =
-		      {
-		        "GameID" : GameID,
-		        "CardNumber" : CardNumber,
-		        "PlayerID" : PlayerID,
-		        "Moves" : 
-		        	
-		      };
-		
-		      var strJSON = JSON.encode(objJSON);
-		      console.log("JSON sent: " + strJSON);
-		      
-		      //create and send the http request
-		      var QueryString = "action=playTurn&content=" + strJSON;
-		      req.open("PUT", "/bullygame/game?" + QueryString, true);
-
-	
-	var objJSON = createQueryObj('playTurn',true,true,true);
-	objJSON.Moves = {
-		"PawnNumber" : pNumberArray,
-		"StartSpace" : pStartArray,
-		"EndSpace" : pEndArray
-	};
-	
-	myRequest.set('send', objJSON);	
-	myRequest.put();	
-}
-
-
-// Helper function for creating querystrings. Currently uses Cookies for the main data.
-function createQueryObj(actionStr,inclGameId,inclPlayerId,inclTurnNumber) {
-	return { 
-		action: actionStr,
-		GameId: inclGameId ? (Cookie.read("gameid" : null),
-		PlayerId: inclPlayerId ? (Cookie.read("playerid" : null),
-		TurnNumber: inclTurnNumber ? (Cookie.read("turnnumber" : null)
-	};
-}	
-
 
 //function for fetching query parameters (from http://www.netlobo.com/url_query_string_javascript.html)
 function gup( name )
@@ -143,4 +39,205 @@ function gup( name )
     return "";
   else
     return results[1];
+}
+
+//used to get the game info (player#, game id, turn #) and put them in cookies
+function getGameInfo()
+{
+	// Create Request
+	var jsonRequest = new Request.JSON({
+	    url: '/bullygame/game',
+	    onSuccess: function(reponseJSON){
+            //update the cookies
+            TurnNumber = Cookie.write("turnnum", reponseJSON.TurnNumber);
+	    },
+	    onFailure: function(){
+	        alert("Error communicating with server.");
+	    }
+	});
+	// Create JSON object
+    var objJSON = createBaseJSONObj();
+    // Send Request
+	jsonRequest.get({
+		'action' : 'GetGameInfo',
+		'content' : JSON.encode(objJSON)
+	});
+}
+
+//function called when the page initially loads to join a game
+function joinGame()
+{
+	var GameID = Cookie.read("gameid");
+    var PlayerID = Cookie.read("playerid");
+    var TurnNumber = Cookie.read("turnnum");
+    if ((GameID==null) || (PlayerID==null))
+	{
+    	// Create Request
+    	var jsonRequest = new Request.JSON({
+    	    url: '/bullygame/game',
+    	    onSuccess: function(reponseJSON){
+                if(!reponseJSON.Status) {
+    	            //create the new cookies
+    	            GameID = Cookie.write("gameid", reponseJSON.GameID);
+    	            PlayerID = Cookie.write("playerid", reponseJSON.PlayerID);
+    	            TurnNumber = Cookie.write("turnnum", reponseJSON.TurnNumber);
+    	
+    	            //update the number of players
+    	            var TotalPlayers = document.getElementById("numplayers");
+    	            TotalPlayers.innerHTML = reponseJSON.NumberPlayers;
+            	} else {
+                	alert("There are no openings in this game, please join a new one.");
+            	}
+    	    },
+    	    onFailure: function(){
+    	        alert("Error communicating with server.");
+    	    }
+    	});
+  	  	//get the player's username from the query string	
+  	  	console.log("fetch username");
+  	  	var username = gup("username") || "anonymous";
+  	  	console.log("username = " + username);
+  	  
+  	  	//update the player name on the board
+        $('playername').set('html',username);
+  	  
+		  //create the json object query string
+		var objJSON = {
+			'PlayerID' : username
+		};
+		// Send Request
+		jsonRequest.post({
+			'action' : 'GetGameInfo',
+			'content' : JSON.encode(objJSON)
+		});
+	} else {
+    	getGameInfo();
+	}
+}
+
+
+//Helper function to create JSON objects
+function createBaseJSONObj() {
+	return {
+	  "GameID" : Cookie.read("gameid"),
+	  "PlayerID" : Cookie.read("playerid"),
+	  "TurnNumber" : Cookie.read("turnnum")
+	};
+}
+
+//checks the cookies to make sure they didn't expire
+function checkCookies()
+{
+	//if cookies not found, ask servlet for game id and player id
+	var GameID = Cookie.read("gameid");
+    var PlayerID = Cookie.read("playerid");
+    var TurnNumber = Cookie.read("turnnum");
+	if ((GameID==null) || (PlayerID==null))
+	{
+		getGameInfo();
+	}
+}
+
+//sends request to the server to draw a card
+function getCard()
+{
+	// Create Request
+	var jsonRequest = new Request.JSON({
+	    url: '/bullygame/game',
+	    onSuccess: function(reponseJSON){
+            //create the new html from the json's data
+            var CardURL = reponseJSON.CardURL;
+            var DiceRoll1 = reponseJSON.DiceRoll1;
+            var DiceRoll2 = reponseJSON.DiceRoll2;
+            
+            //display a new card
+            //$('NewCard').src = CardURL;
+
+            //update the dice
+            setDice(DiceRoll1,DiceRoll2);            
+	    },
+	    onFailure: function(){
+	        alert("Error communicating with server.");
+	    }
+	});
+	// Create JSON object
+    var objJSON = createBaseJSONObj();
+    // Send Request
+	jsonRequest.get({
+		'action' : 'GetCard',
+		'content' : JSON.encode(objJSON)
+	});
+}
+
+//sends a request to the server to use a card
+function playTurn()
+{
+	// Create Request
+	var jsonRequest = new Request.JSON({
+	    url: '/bullygame/game',
+	    onSuccess: function(reponseJSON){
+            //create the new html from the json's data
+            var Status = reponseJSON.Status;
+            
+            //display a new card
+            if (Status=="Valid") {
+            	alert("roll was validated by the server.")
+        	}
+	    },
+	    onFailure: function(){
+	        alert("Error communicating with server.");
+	    }
+	});
+	
+    //get pawn moves
+    var pNumberArray = [1];
+    var pStartArray = [0];
+    var pEndArray = [5];
+    
+	// Create JSON object
+    var objJSON = createBaseJSONObj();
+    objJSON.CardNumber = 1;
+    objJSON.Moves = {
+		"PawnNumber" : pNumberArray,
+		"StartSpace" : pStartArray,
+		"EndSpace" : pEndArray
+    };
+    
+    // Send Request
+	jsonRequest.get({
+		'action' : 'playTurn',
+		'content' : JSON.encode(objJSON)
+	});
+}
+
+//sends a request to the server to see if the game state has changed
+function checkState()
+{
+	// Create Request
+	var jsonRequest = new Request.JSON({
+	    url: '/bullygame/game',
+	    onSuccess: function(reponseJSON){
+            //create the new html from the json's data
+            var Status = reponseJSON.Status;
+            
+            //update the turn
+            var TurnNumber = document.getElementById("turn");
+            TurnNumber.innerHTML = reponseJSON.TurnNumber;
+            
+            //check to see if something was changed
+            if (Status=="State Changed") {
+            	alert("There has been a change")
+        	}	    
+	    },
+	    onFailure: function(){
+	        alert("Error communicating with server.");
+	    }
+	});
+	// Create JSON object
+    var objJSON = createBaseJSONObj();
+    // Send Request
+	jsonRequest.get({
+		'action' : 'CheckState',
+		'content' : JSON.encode(objJSON)
+	}); 
 }
